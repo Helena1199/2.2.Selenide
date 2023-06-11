6363d42b9004243f658e558e2827effffc63bad3
Helena <v0robjeva.alena@yandex.ru>
2023-06-11 22:13:28 +0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/xw8gwgc6b89re4d6/branch/main?svg=true)](https://ci.appveyor.com/project/Helena1199/2-2-selenide/branch/main)
---
+++ src/test/java/ru/netology/selenide/TestCardDelivery.java
@@ -0,0 +1,38 @@
+package ru.netology.selenide;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+class TestCardDelivery {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    void shouldTestVPositive() {
+        open("http://localhost:9999");
+        $("span[data-test-id=city] input").setValue("Москва");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        $("span[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("span[data-test-id=date] input").sendKeys(currentDate);
+        $("span[data-test-id=name] input").setValue("Василий");
+        $("span[data-test-id=phone] input").setValue("+79012345678");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $x("//div[contains(text(), 'Встреча успешно забронирована на ')]").shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
