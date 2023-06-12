60ecb0e9506cc7e06ced40b84cb9197b4c1bcf1f
Helena <v0robjeva.alena@yandex.ru>
2023-06-12 15:52:14 +0300

    chore: initial commit 7

--- src/test/java/ru/netology/selenide/TestCardDelivery.java
+++ src/test/java/ru/netology/selenide/TestCardDelivery.java
@@ -1,15 +1,12 @@
 package ru.netology.selenide;
 
 
-import com.codeborne.selenide.Browser;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 import com.codeborne.selenide.Configuration;
